7e041983d7aa1e539d64a0256b4909b1a25cacd4
Tar <tarsiciosh@gmail.com>
2020-08-14 15:13:20 -0300

    MAC ATOM

--- App.js
+++ App.js
@@ -1,36 +1,168 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { View, Text, Button, TextInput, AsyncStorage } from 'react-native';
+import { NavigationContainer, TabActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+//import ContactListScreen from './screens/ContactListScreen';
+//import AddContactScreen from './screens/AddContactScreen';
+//import contacts from './contacts'
+//import {fetchUsers} from './api'
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator(); 
 
-export default function App() {
+const AuthContext = React.createContext();
+
+export default function App ({navigation}) {
+  const [state, dispatch] = React.useReducer (
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          }
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          }
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          }
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  )
+  
+  React.useEffect( () => {
+    // Fetch the token from storage then navigate to our appropriate place
+    const bootstrapAsync = async () => {
+      let userToken
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch (e) {
+        //Restoring token failed
+      }
+      // After restoring token, we may need to validate it in production app
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      dispatch({type: 'RESTORE_TOKEN', token: userToken})
+    }
+    bootstrapAsync()
+  },[])
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async data => {
+        // In a production app, we need to send some data (usually username, password) to server and get a token
+        // We will also need to handle errors if sign in failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        console.log(data.username)
+        console.log(data.password)
+ 
+        /*
+        fetch('http://localhost:8000',{
+          method:'POST',
+          headers: {'content-type': 'application/json'},
+          body: JSON.stringify({username: data.username, password: data.password})
+        }).then( (res) => {console.log(res)})
+        */
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token'})
+      },
+      signOut: () => dispatch ({ type: 'SIGN_OUT'}),
+      signUp: async data => {
+        // In a production app, we need to send user data to server and get a token
+        // We will also need to handle errors if sign up failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+        dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'})
+      },
+    }),
+    []
+  )
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
-  );
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+      <Stack.Navigator>
+        {state.userToken == null ? (
+          <Stack.Screen name="SignIn" component={SignInScreen} />
+        ) : (
+          <Stack.Screen name="Home" component={HomeScreen} />
+        )}
+      </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+function HomeScreen ({navigation}) {
+  return (
+    <View>
+      <Text>Hello mongo</Text>
+      <Button
+        title="goBack"
+        onPress= {() => navigation.navigate('SignIn')}
+      />
+    </View>
+  )
+}
+
+function SignInScreen () {
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
+
+  const {signIn} = React.useContext(AuthContext)
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button 
+        title="Sign in" 
+        onPress={() => signIn({username,password})} 
+      />
+    </View>
+  )
+}
+
+/*
+<Stack.Navigator>
+          <Stack.Screen
+            name='ContactList'
+            component={ContactListScreen}
+            initialParams={{
+              contacts: this.state.contacts,
+            }}
+          />
+        <Stack.Screen 
+          name='AddContact'
+          component={AddContactScreen}
+          initialParams={{
+            addContact : ( () => this.addContact )
+            }}           
+          />
+        </Stack.Navigator>
+
+ */
